74699b49bcd0221e4bf20f667f9825116230ee5f
ucmit <ulan@cmit.ru>
2020-09-20 11:14:50 +0700

    i

---
+++ JQuery/1. JQuery/script.js
@@ -0,0 +1,5 @@
+
+console.log($(".circle").css("background-color"))
+
+$(".circle").css("background-color")
+document.getElementsByClassName("circle")[0].style.backgroundColor;
---
+++ Javascript/final JS/script.js
@@ -0,0 +1,44 @@
+function onSubmit(event){
+    // Отменить событие
+    event.preventDefault();
+
+    // Переданная форма
+    let form = event.target;
+    // Получаем информацию пользователя
+    let name = form.userName.value;
+    let age = form.userAge.value;
+    let from = form.userFrom.value;
+
+    let sA =  "Жил-был, не тужил, семки грыз и много ел " + name + ". ";
+    let sB = "Однажды он съел ровно " + age + " килограмм шаурмы."
+    let sC = "Все были настолько поражены, что его взяли в элитную группу " + from;
+
+    
+
+    // Взятие элементы чек-бокса по имени класса
+    // userLike не массив D:
+    let userLike = document.getElementsByClassName("userLike");
+    // Создаём пусто массив
+    let like = [];
+
+    // Перебираем массив userLike
+    for(let i = 0; i<userLike.length; i++){
+        // Если есть галочки
+        if(userLike[i].checked == true){
+            // Добавляем значение input в массив like
+            like.push(userLike[i].value)
+        }
+    }
+
+    // Что любит пользователь?
+    let sD = "";
+    
+
+
+
+    story.innerHTML = sA + sB + sC + sD;
+}
+
+
+popitka.addEventListener("submit", onSubmit);
+
