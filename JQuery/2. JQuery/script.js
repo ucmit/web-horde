06076690626fad46c72d1d94edfb2ed7e6afccc9
ucmit <ulan@cmit.ru>
2020-10-11 18:34:24 +0700

    memo game

--- JQuery/2. JQuery/script.js
+++ JQuery/2. JQuery/script.js
@@ -1,15 +1,15 @@
 // ************** Параметры игры ******************
+let firstCard = null;
+let secondCard = null;
+let wait = false; 
 
 // Готовый алгоритм перебора карт
 function shuffle(array) {
     let copy = [], n = array.length, i;
-  
     // While there remain elements to shuffle…
     while (n) {
-  
       // Pick a remaining element…
       i = Math.floor(Math.random() * array.length);
-  
       // If not already shuffled, move it to the new array.
       if (i in array) {
         copy.push(array[i]);
@@ -17,30 +17,33 @@ function shuffle(array) {
         n--;
       }
     }
-  
     return copy;
 }
 
 // Функция генерации игрового поля
 function generateField(size){
-    
+    // База типов задних карточек 
     let cls = ["c1", "c2", "c3", "c4"]
     let counter = 0;
+    // Массив задних карточек
     let backs = [];
+    // Заполняем массив парами из базы типов карточек
     for(let i=0; i<(size*size); i+=2){
         backs[i] = cls[counter];
         backs[i+1] = cls[counter];
-        
         if(counter < 3){
             counter++;
         }
         else counter = 0;
     }
 
-    console.log(backs)
-
+    // Проводим шафл для нашего массива карточек
+    backs = shuffle(backs);
+    
+    // Массив элементов карточек
     let cards = [];
     for(let i=0; i<(size*size); i++){
+
         let card = $("<div>");
         $(card).addClass("card");
 
@@ -51,7 +54,7 @@ function generateField(size){
         $(front).addClass("front");
 
         let back = $("<div>");
-        $(back).addClass("back");
+        $(back).addClass("back").addClass(backs[i]);
 
         $(flipper).append(back)
         $(flipper).prepend(front)
@@ -68,7 +71,50 @@ function generateField(size){
     })
     // Добавляем в наше поле карточки
     $("#field").append(cards);
+
+    // Переворот картинки при нажатии
+    $(".flipper").click(function(event){ 
+        if(wait == false  && firstCard  != event.currentTarget){
+            $(event.currentTarget).toggleClass("clicked");
+            checkCard(event.currentTarget);
+        }
+    });
 }
+
+// Основная функция игры
+function checkCard(card){
+    // Выбираем первую карточку
+    if(firstCard == null){
+        firstCard = card;
+    }
+    else if(secondCard == null){
+        // Выбираем вторую карточку
+        secondCard = card;
+        // Запрещаем переворачивать карты
+        wait = true;
+
+        // Проверяем 1000мс
+        setTimeout(function(){
+            //  Проводим сравнение карточек
+            // Нашли одинаковые карточки
+            if(  $(firstCard).children(".back").attr("class")  == $(secondCard).children(".back").attr("class")    ){
+                $(firstCard).css("visibility", "hidden");
+                $(secondCard).css("visibility", "hidden");
+            }
+            else{
+                // Переворачиваем карточки обратно
+                $(".flipper").removeClass("clicked");
+            }
+
+            // Забываем карточки и разрешаем переворачивать карты
+            firstCard = null;
+            secondCard = null;
+            wait = false;
+        }, 1000)
+    }
+}
+
+
 
 // Нажатие 
 $("#btn").click(function(event) { 
@@ -84,9 +130,5 @@ $("#btn").click(function(event) {
 
 
 
-// Переворот картинки при нажатии
-$(".flipper").click(function(event){ 
-    $(event.currentTarget).toggleClass("clicked");
-    
-});
+
 
